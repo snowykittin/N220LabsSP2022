bd4146c40cc19fd84f2e0b0e8ab5088f1704be34
Summer S <snowykittin@gmail.com>
2022-04-26 19:36:21 -0400

    Completion

--- Notes/FinalProject/js/app.js
+++ Notes/FinalProject/js/app.js
@@ -1,8 +1,12 @@
 let dvCard = document.getElementsByClassName("card");
+let dvPairResult = document.getElementById("pairInfo");
 let pairFound = 0;
 let clickCount = 0;
 let tagOne = "";
 let tagTwo = "";
+let firstCardElement = null;
+let secondCardElement = null;
+
 
 for(let i = 0; i < dvCard.length; i++){
     dvCard[i].addEventListener("click", flipCard);
@@ -11,19 +15,23 @@ for(let i = 0; i < dvCard.length; i++){
 function flipCard(event){
     //First checks for win condition.
     if(pairFound == 8){
-        console.log("You've won!");
+        dvPairResult.innerHTML = "You've Won!";
         //Resets pairFound
         pairFound = 0;
     }else{
         //sets first click
         if(clickCount == 0){
+            firstCardElement = event.target;
             tagOne = event.target.getAttribute("data-tag");
             event.target.innerHTML = event.target.getAttribute("data-tag");
+            event.target.classList.toggle("hidden");
             clickCount++;
         }else{
             //sets second click
+            secondCardElement = event.target;
             tagTwo = event.target.getAttribute("data-tag");
             event.target.innerHTML = event.target.getAttribute("data-tag");
+            event.target.classList.toggle("hidden");
     
             clickCount = 0;
             //sends them to be checked
@@ -38,12 +46,17 @@ function checkCards(cardOne, cardTwo){
         console.log("Match!");
         //adds one to pairFound for win condition
         pairFound++;
-        tagOne = "";
-        tagTwo = "";
+        firstCardElement.classList.toggle("removed");
+        secondCardElement.classList.toggle("removed");
     }else{
-        //resets what tagOne and tagTwo are.
-        console.log("Try again.");
-        tagOne = "";
-        tagTwo = "";
+        //resets cards back to hidden after two seconds
+        setTimeout(function (){
+            console.log("Try again.");
+        firstCardElement.classList.toggle("hidden");
+        secondCardElement.classList.toggle("hidden");
+        },2000);
     }
+
+    //updates Pairs Found.
+    dvPairResult.innerHTML = "Pairs Found: " + pairFound;
 }
